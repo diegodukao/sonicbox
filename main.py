f2749fc3611b4ab9a37fa61a421c108e4f75a6f5
Diego Moreira Guimarães <diegodukao@gmail.com>
2018-03-13 14:45:58 -0300

    playing tabla drum samples

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
 from pythonosc import udp_client
 
-from samples import AMBIENT_SOUNDS, sample_path
+from samples import sample_path, SOUNDS_TABLA_DRUM as SAMPLES
 
 kivy.require('1.10.0')
 
@@ -15,9 +15,9 @@ class SamplesKeyboard(GridLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-        self.cols = 2
+        self.cols = 3
 
-        for sample in AMBIENT_SOUNDS:
+        for sample in SAMPLES:
             btn = PlayButton(text=sample)
             self.add_widget(btn)
 
