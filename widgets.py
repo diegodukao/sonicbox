90cedd8ec48a7c6016d517787c5fe67f06cff7dc
Diego Moreira Guimarães <diegodukao@gmail.com>
2018-03-21 12:35:25 -0300

    Synths screen initial version

--- widgets.py
+++ widgets.py
@@ -1,8 +1,31 @@
+from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.screenmanager import Screen
 from kivy.uix.stacklayout import StackLayout
 from kivymd.button import MDRaisedButton
 from pythonosc import udp_client
 
 from samples import sample_path
+
+
+class SamplesScreen(Screen):
+    pass
+
+
+class SynthsScreen(Screen):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self.name = 'synths'
+
+        grid = GridLayout()
+        grid.cols = 8
+
+        for i in range(64):
+            grid.add_widget(Button())
+
+        self.add_widget(grid)
 
 
 class SamplesKeyboard(StackLayout):
