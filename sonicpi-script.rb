8cff0d013392c501abaccb1c772a2b141c7fb125
Diego Moreira Guimarães <diegodukao@gmail.com>
2018-04-11 23:48:30 -0300

    sonic pi script added

---
+++ sonicpi-script.rb
@@ -0,0 +1,22 @@
+# Run this script in a Sonic Pi buffer for Sonicbox to work
+
+
+use_real_time
+
+live_loop :samples do
+  name = sync "/osc/sample"
+  sample name
+end
+
+live_loop :synths do
+  synth_name, tonic, name, note_num = sync "/osc/synth"
+  use_synth synth_name.to_sym
+  play scale(tonic, name, num_octaves: 7)[note_num]
+end
+
+live_loop :drum_machine do
+  set_list = sync "/osc/drum-machine"
+  set_list.each do |set|
+    sample :loop_amen, onset: set
+  end
+end
