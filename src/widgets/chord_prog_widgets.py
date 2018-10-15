b5c1d3bd8b1c620907ed092a1d01147df7c1712a
Diego Moreira Guimarães <diegodukao@gmail.com>
2018-10-15 14:18:21 -0300

    ChordProg: ChordsSelector interface 1st version

--- src/widgets/chord_prog_widgets.py
+++ src/widgets/chord_prog_widgets.py
@@ -1,4 +1,5 @@
 from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import Screen
 
 
@@ -6,4 +7,12 @@ Builder.load_file('ui/chord_prog_screen.kv')
 
 
 class ChordProgScreen(Screen):
+    pass
+
+
+class ChordsSelector(BoxLayout):
+    pass
+
+
+class ChordsColumn(BoxLayout):
     pass
